3f15f4991bf57ef6fe229797aeb919b76c5e315b
Mohorang <dlwnsgh253@gmail.com>
2022-08-04 17:20:23 +0900

    백준 10828문제 스택구현하기

---
+++ beek10828/src/Main.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] stack;
+    public static int size=0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        stack = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            switch(st.nextToken()){
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    System.out.println(pop());
+                    break;
+                case "size":
+                    System.out.println(size());
+                    break;
+                case "empty":
+                    System.out.println(empty());
+                    break;
+                case "top":
+                    System.out.println(top());
+                    break;
+            }
+        }
+    }
+    public static void push(int num){
+        stack[size] = num;
+        size++;
+    }
+    public static int pop(){
+        if(size==0){
+            return -1;
+        }
+        else{
+            int res = stack[size -1];
+            stack[size -1] = 0;
+            size--;
+            return res;
+        }
+    }
+    public static int size(){
+        return size;
+    }
+    public static int empty(){
+        if(size == 0){
+            return 1;
+        }
+        else return 0;
+    }
+    public static int top(){
+        if(size == 0){
+            return -1;
+        }
+        else {
+            return stack[size-1];
+        }
+    }
+
+}
